841cf251a18377a7c9b981613cf7353e04519970
Lukas Grahl <lukas.g@amkat.de>
2023-03-22 19:22:24 +0100

    added cache for loading data to app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.express as px
@@ -8,34 +9,75 @@ import os
 
 from settings import DATA_DIR
 
+from src.pull_data import get_yf_ticker_data, get_sp500_n_largest, load_csv, test_res_cache
+from src.filter import get_ARMA_test, set_up_kalman_filter, kalman_filter
+
 if __name__ == '__main__':
+    sp500_ticker = ['^GSPC']
+    start = '2020-01-01'
+    end = '2022-12-31'
+
     # streamlit setup
     st.set_page_config(page_title='A binary guide to the S&P 500', layout='wide')
 
     # Part 1
     st.header('SP 500 price overview')
-    a1, a2 = st.columns([1, 1])
-    b1, b2, b3 = st.columns([1, 2, 1])
+    st.write(
+       f"""
+        The past daily prices from {start} to {end}
+        """
+    )
+    a = st.columns(1)
 
-    # Part 2
-    st.header('Risk Portfolio Allocation')
-    d1, d2, d3, d4 = st.columns([1, 1, 1, 1])
-    e1, e2 = st.columns([1, 1])
+    clear_cache = st.button('Clear cache')
 
     # Load data
-    sp500_ticker = ['^GSPC']
-    start = '2020-01-01'
-    end = '2022-12-31'
+    sp500_largest = get_sp500_n_largest()
+    df_prices = load_csv('prices.csv', DATA_DIR, time_period='D', index_col='date')
+    df_rets = np.log(df_prices / df_prices.shift(1)).dropna().copy()
 
-    df_rets = pd.read_csv(os.path.join(DATA_DIR, 'returns.csv'), index_col='date').drop('Unnamed: 0', axis=1)
-    fig1 = px.line(df_rets[sp500_ticker])
-    fig2 = px.histogram(df_rets[sp500_ticker])
+    # ARIMA
+    endog = ['^GSPC']
+    exog = ['AAPL', 'MSFT', 'GOOG', 'GOOGL', 'AMZN']
+    p, q = 2, 1
 
-    tab1, tab2 = st.tabs(["Line plot", "Histogram"])
+    # get SP500 lead returns for prediction
+    df_rets[f'{endog[0]}_lead'] = df_rets[endog[0]].shift(1)
+    df_rets.dropna(inplace=True)
+
+    # get arima output
+    p, q, d, ma_resid, arima_params = get_ARMA_test(p, q, df_rets, endog, exog)
+
+    # set up filter
+    xdim = p + d + q
+    zdim = xdim
+
+    T, Q, Z, H, x0, P0, zs, state_vars = set_up_kalman_filter(p, q, d, xdim, zdim,
+                                                              df_rets, ma_resid, arima_params, endog, exog)
+    X_out, P_out, X_pred, P_pred, LL_out = kalman_filter(xdim, zdim, p, q, d, x0, P0, zs, T, Q, Z, H, state_vars)
+
+    # get kalman prediction into df
+    df_xpred = pd.DataFrame(data=X_pred[:, 0], index=df_rets.index[:len(X_pred)], columns=['predicted'])
+    df_xpred = df_xpred.join(df_rets[endog])
+    # test cache
+    # _ = test_res_cache()
+
+    # plots
+    fig1 = px.line(df_prices[sp500_ticker].values)
+    fig2 = px.line(X_pred[:, 0])
+    # fig2 = px.line(x=df_xpred.index, y=df_xpred['predicted'])
+    # fig2.add_scatter(x=df_rets.index, y=df_rets[endog[0]])
+    fig3 = px.line(df_rets[endog[0]].values)
+
+    # fill streamlit page
+    st.plotly_chart(fig1, theme='streamlit', use_container_width=True)
+
+    st.header('Forecasting output')
+    tab1, tab2 = st.tabs(["Kalman Filter", "ARIMA"])
     with tab1:
-        st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
+        st.line_chart(df_xpred)
     with tab2:
-        st.plotly_chart(fig2, theme=None, use_container_width=True)
+        st.plotly_chart(fig3, theme='streamlit', use_container_width=True)
 
-    # st.plotly_chart(fig1, title=f'{sp500_ticker[0]} returns')
-    # st.ploty_chart(fig2, title=f'{sp500_ticker[0]} returns histogram')
+    if clear_cache:
+        st.cache_data.clear()
--- src/pull_data.py
+++ src/pull_data.py
@@ -1,13 +1,27 @@
+import numpy as np
 import pandas as pd
 import pandas_datareader as pdread
 from yahoo_fin import stock_info as ysi
 import yfinance as yf
 
+import os
+import time
+
 from src.utils import apply_datetime_format
 
+import streamlit as st
 
-def get_yf_ticker_data(tickers: list, start: str, end: str, price_kind: str = 'Adj Close'):
-    # price_kind: Open, Close, Low, High, Volume
+
+@st.cache_data()
+def get_yf_ticker_data(tickers: list, start: str, end: str, price_kind: str = 'Adj Close') -> pd.DataFrame:
+    """
+    Pull data from yahoo finance from tickers
+    :param tickers: list of yfinance ticker
+    :param start: start date in '%Y-%m-%d' format
+    :param end: end date in '%Y-%m-%d' format
+    :param price_kind: choose between Open, Close, Low, High, Volume
+    :return: df of tickers and price_kind
+    """
     df_prices = pd.DataFrame()
     df_prices.index = pd.date_range(start, periods=(
             apply_datetime_format(end, '%Y-%m-%d') - apply_datetime_format(start, '%Y-%m-%d')).days)
@@ -25,7 +39,13 @@ def get_yf_ticker_data(tickers: list, start: str, end: str, price_kind: str = 'A
     return df_c
 
 
-def get_sp500_n_largest(n: int = 5):
+@st.cache_data()
+def get_sp500_n_largest(n: int = 5) -> list:
+    """
+    Pull list of SP 500 composits and their market cap to obtian n largest
+    :param n: number of largest composits by market cap
+    :return: list n yfinance tickers
+    """
     # get sp500 composits & market cap
     sp500_tickers = ysi.tickers_sp500()
     df = pd.DataFrame(index=sp500_tickers,
@@ -33,3 +53,27 @@ def get_sp500_n_largest(n: int = 5):
                       data=[pdread.get_quote_yahoo(item)['marketCap'].values[0] for item in sp500_tickers])
     sp500_largest = df.sort_values('market_cap', ascending=False).index[:n].values
     return sp500_largest
+
+
+# @st.cache_data()
+def load_csv(file_name: str, path: str, time_period: str = None, **kwargs) -> pd.DataFrame:
+    df = pd.read_csv(os.path.join(path, file_name), **kwargs)
+    if time_period is not None:
+        df.index = pd.DatetimeIndex(df.index).to_period(time_period)
+    return df
+
+
+@st.cache_resource()
+def test_res_cache():
+    time.sleep(10)
+    return True
+
+
+# def get_returns_of_prices(prices: pd.DatFrame):
+#     return np.log(prices / prices.shift(1)).dropna().sort_index()
+
+def train_test_split(df_in: pd.DataFrame, test_size) -> (pd.DataFrame, pd.DataFrame):
+    df = df_in.copy()
+    test_ind = int(len(df) * test_size)
+    # df['test_set'] = [*chain([False] * (len(df) - test_ind), [True] * test_ind)]
+    return df.iloc[:test_ind], df[test_ind:]
