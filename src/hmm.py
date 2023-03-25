80d24ebe284f8d062df34c2e3687280fdd79e59b
Lukas Grahl <lukas.g@amkat.de>
2023-03-25 13:05:42 +0100

    added hmm package

---
+++ src/hmm.py
@@ -0,0 +1,146 @@
+import numpy as np
+
+from src.utils import printProgBar
+from random import randint
+from itertools import chain
+
+from sklearn.mixture import GaussianMixture
+import pandas as pd
+from src.utils import get_index
+import matplotlib.pyplot as plt
+import streamlit as st
+
+
+def get_hmm_features(arr: np.array, ind_ticker: str, cols_list: list, n_largest_stocks):
+    x = arr[:, get_index(f'{ind_ticker}_Volume', cols_list)]
+    vol_gap = np.diff(x) / x[1:]
+
+    # daily_change
+    d_change = ((arr[:, get_index(ind_ticker, cols_list)]
+                 - arr[:, get_index(f'{ind_ticker}_Open', cols_list)])
+                / arr[:, get_index(f'{ind_ticker}_Open', cols_list)])[1:]
+
+    # fract_high
+    frac_high = ((arr[:, get_index(f'{ind_ticker}_High', cols_list)]
+                  - arr[:, get_index(f'{ind_ticker}_Open', cols_list)])
+                 / arr[:, get_index(f'{ind_ticker}_Open', cols_list)])[1:]
+
+    # fract_low
+    frac_low = ((arr[:, get_index(f'{ind_ticker}_Low', cols_list)]
+                 - arr[:, get_index(f'{ind_ticker}_Open', cols_list)])
+                / arr[:, get_index(f'{ind_ticker}_Open', cols_list)])[1:]
+
+    # n largest stocks
+    x = [get_index(item, n_largest_stocks) for item in n_largest_stocks]
+    n_largest = [arr[:, x[i]][1:] for i in x]
+
+    # forecast_variable
+    forecast = arr[:, get_index(ind_ticker, cols_list)][1:]
+
+    out = [*chain(
+        [
+            vol_gap,
+            d_change,
+            frac_high,
+            frac_low,
+            forecast
+        ],
+        [item for item in n_largest]
+    )]
+
+    return out
+
+
+@st.cache_resource()
+def get_CV_data(data_arr: np.array, cols_list: list, ind_ticker: str, n_largest_stocks: list,
+                n_iterations: int, sample_size: tuple = (10, 30)) -> (np.array, list):
+    quotes = []
+
+    for i in range(0, n_iterations):
+        printProgBar(i, n_iterations - 1)
+
+        # random sample len
+        sample_len = randint(sample_size[0], sample_size[1])
+        # random sample start
+        sample_start = randint(0, len(data_arr) - sample_len)
+        subset = data_arr[sample_start: sample_start + sample_len]
+
+        # generate features
+        # close_date
+        # close_date = np.max(subset[:, get_index('date', cols_list)])
+
+        # volume_gap
+
+        features = get_hmm_features(subset, ind_ticker, cols_list, n_largest_stocks)
+
+        # append
+        out_len = len(subset) - 1
+        features.insert(0, [i] * out_len)
+        out = np.array(features, dtype='object')
+        quotes.append(out)
+
+    # user defined, depending on
+    ret_cols = [
+        *chain(
+            [
+                'id',
+                'volume_gap',
+                'daily_change',
+                'fract_high',
+                'fract_low',
+                'forecast_variable'
+            ],
+            n_largest_stocks
+        )
+    ]
+    return quotes, ret_cols
+
+
+@st.cache_resource()
+def get_hmm(X_train, y_train, n_components: int, cov_type: str = 'full', n_int: int = 100, random_state=101):
+    mod = GaussianMixture(n_components=n_components, covariance_type=cov_type, n_init=n_int, random_state=random_state)
+    mod.fit(X_train, y_train)
+    hidden_states = mod.predict(X_train)
+    return mod, hidden_states
+
+
+def get_hidden_states(hidden_states, y_train):
+    states = np.zeros((len(y_train), 2))
+    states[:, 0] = hidden_states
+    states[:, 1] = y_train
+    states = pd.DataFrame(states, columns=['states', 'rets'])
+    statesg = states.groupby('states').agg({'rets': ['mean', 'std',
+                                                     lambda x: np.mean(x) + 1.96 * np.std(x),
+                                                     lambda x: np.mean(x) - 1.96 * np.std(x)]}).rename(
+        columns={'<lambda_0>': 'conf_lower',
+                 '<lambda_1>': 'conf_upper'}
+    )
+    statesg.columns = statesg.columns.get_level_values(1)
+    return states, statesg
+
+
+def plot_hmm_states(df, y_states, price_col: str, ret_col: str, date_col: str):
+    states = set(list(y_states))
+
+    plt.figure(figsize=(15, 10))
+    plt.subplot(2, 1, 1)
+
+    for i in states:
+        want = (y_states == i)
+        x = df[date_col].iloc[want]
+        y = df[price_col].iloc[want]
+        plt.plot(y, '.')
+    plt.legend(states, fontsize=16)
+    plt.grid(True)
+    plt.xlabel(date_col, fontsize=16)
+    plt.subplot(2, 1, 2)
+    for i in states:
+        want = (y_states == i)
+        x = df[date_col].iloc[want]
+        y = df[ret_col].iloc[want]
+        plt.plot(y, '.')
+    plt.legend(states, fontsize=16)
+    plt.grid(True)
+    plt.xlabel("datetime", fontsize=16)
+    plt.show()
+    pass
---
+++ tests/test_hmm.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+from src.pull_data import get_index_nlargest_composits, get_yf_ticker_data
+from itertools import chain
+from src.utils import train_test_split
+from sklearn.preprocessing import scale
+from src.hmm import get_index
+import matplotlib.pyplot as plt
+from src.hmm import get_hmm, get_hidden_states
+import seaborn as sns
+from src.hmm import plot_hmm_states
+
+
+
+
+from src.hmm import get_CV_data
+
+if __name__ == '__main__':
+    # user input
+    start = '2015-01-01'
+    end = '2022-12-31'
+    sel_ind = 'CAC 40'
+    sel_ind_ticker = ['^FCHI']
+
+    # pull data
+    sel_ind_composit_tickers, _, sel_ind_nlargest_tickers, success = get_index_nlargest_composits(sel_ind, n=5)
+    # if success <= .8: st.write(f'Market cap was only available for {success*100: .1f} %  of composits')
+    df_prices = get_yf_ticker_data(sel_ind_nlargest_tickers, start, end)
+    df_prices = df_prices.join(get_yf_ticker_data(sel_ind_ticker, start, end, price_kind=['Open', 'High', 'Low', 'Volume', 'Adj Close']))
+    df_prices.columns = [item if '_Adj Close' not in item else item[:-10] for item in df_prices.columns]
+    # df_prices = get_yf_ticker_data([*chain(sel_ind_ticker, sel_ind_nlargest_tickers)], start, end)
+
+    # replace zero in volume cols
+    x = df_prices[df_prices['^FCHI_Volume'] == 0].index
+    df_prices.loc[x, '^FCHI_Volume'] = [1] * 56
+
+    # get log return data
+    df_rets = np.log(df_prices / df_prices.shift(1)).dropna().copy()
+
+    # get data for CV
+    data = df_rets.drop([item for item in df_rets.columns if sel_ind_ticker[0] in item], axis=1).copy()
+    data = data.join(df_rets[sel_ind_ticker[0]])
+    data = data.join(df_prices[[f'{sel_ind_ticker[0]}_{item}' for item in ['High', 'Low', 'Open', 'Volume']]])
+    data = data.join(df_prices[sel_ind_ticker].rename(columns={sel_ind_ticker[0]: f'{sel_ind_ticker[0]}_price'}))
+    train, test = train_test_split(data, test_size_split=[.8])
+
+    from src.hmm import get_hmm_features
+    # get cross validation and testing data
+    arr_test = np.array(get_hmm_features(test.values, sel_ind_ticker[0],
+                                         list(test.columns), list(sel_ind_nlargest_tickers)))
+    cv_train, cv_cols = get_CV_data(train.values, list(train.columns), sel_ind_ticker[0],
+                                    n_largest_stocks=list(sel_ind_nlargest_tickers), n_iterations=10_000)
+    # scale data
+    arr_test = arr_test.transpose()
+    arr_train = np.concatenate(cv_train, axis=1).transpose()
+    arr_train = arr_train[:, 1:]
+
+    arr_train = np.column_stack([scale(arr_train[:, i]) for i in range(arr_train.shape[1])])
+    arr_test = np.column_stack([scale(arr_test[:, i]) for i in range(arr_test.shape[1])])
+    X_train, y_train = arr_train[:, :-1], arr_train[:, -1]
+    X_test, y_test = arr_test[:, :-1], arr_test[:, -1]
+
+    # train model
+    mod, hidden_states = get_hmm(X_train, y_train, n_components=3, n_int=100)
+    states, statesg = get_hidden_states(hidden_states, y_train)
+
+    for i in set(states['states']):
+        plt.hist(states[states['states'] == i]['rets'], bins='fd', alpha=.6, label=i)
+    plt.legend()
+    plt.show()
+
+    sns.violinplot(states, x='states', y='rets')
+    plt.plot([-1, 0, 1, 2, 3], [0, 0, 0, 0, 0], color='black')
+    plt.show()
+
+    hidden_states = mod.predict(X_test)
+    X_test = pd.DataFrame(X_test, columns=cv_cols[1:-1])
+    X_test[f'{sel_ind_ticker[0]}_price'] = test[f'{sel_ind_ticker[0]}_price'].iloc[1:].values
+    X_test[f'{sel_ind_ticker[0]}'] = test[f'{sel_ind_ticker[0]}'].iloc[1:].values
+    X_test['date'] = list(test.index)[1:]
+    plot_hmm_states(X_test, hidden_states, f'{sel_ind_ticker[0]}_price', f'{sel_ind_ticker[0]}', 'date')
