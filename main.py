63109d3350de8e499d8643b0a44a0f33127477fe
Lukas Grahl <lukas.g@amkat.de>
2023-04-10 09:57:32 +0200

    added arma

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.toml import load_toml
 from src.pull_data import load_data
 from src.filter import get_kalman_cv, run_kalman_filter
+from src.arma import grid_search_arma
 from src.utils import is_outlier
 from src.hmm import run_hmm
 import os
@@ -28,19 +29,37 @@ if __name__ == "__main__":
 
     DF_PRICES, DF_RETS, SEL_IND_NLARGEST_TICKERS, LEAD_NAME = load_data(SEL_IND, SEL_IND_TICKER,
                                                                         str(PULL_START_DATE), str(PULL_END_DATE),
-                                                                        no_internet=True)
-    hmm_states, cv_samples, hmm_init, dt_start, measurement_noise, analysis_time, cv_samples_kalman = view.set_up_sliders()
+                                                                        n_largest=config['data']['n_largest_composits'])
+                                                                        # no_internet=True<)
+    _ = view.set_up_sliders()
+    hmm_states, cv_samples, hmm_init, test_sample_start, measurement_noise, analysis_time, cv_samples_kalman = _
+
+    #### ARMA ####
+    from datetime import timedelta
+    from src.arma import get_ARMA_test
+    ind = (test_sample_start - timedelta(config['default_values']['arma_analaysis_time_days']))
+    p, q, d, ma_residuals, arma_params, arma_mod = grid_search_arma(config['data']['p_max'], config['data']['q_max'],
+                                                                    DF_RETS.loc[ind: test_sample_start], endog=[LEAD_NAME],
+                                                                    exog=SEL_IND_NLARGEST_TICKERS.copy(), sup_warnings=True)
+
+    _, _, _, _, _, arma_mod = get_ARMA_test(p, q, DF_RETS.loc[test_sample_start:].iloc[:-1], endog=[LEAD_NAME],
+                                            exog=SEL_IND_NLARGEST_TICKERS.copy(), sup_warnings=True, vals_only=True)
+    arma_predict= arma_mod.predict()
+    arma_true = DF_RETS.loc[test_sample_start:, ].iloc[:-1][SEL_IND_TICKER].values.reshape(-1)
 
     #### Kalman Filter ####
     # cross validate kalman filter
-    kf_conf_mat, kf_roc_score = get_kalman_cv(data=DF_RETS, endog=[LEAD_NAME], exog=SEL_IND_NLARGEST_TICKERS.copy(),
+    kf_conf_mat, kf_roc_score = get_kalman_cv(data=DF_RETS.loc[:test_sample_start], endog=[LEAD_NAME],
+                                              exog=SEL_IND_NLARGEST_TICKERS.copy(),
                                               measurement_noise=measurement_noise,
-                                              cv_index_len=DF_PRICES.loc[PULL_START_DATE: dt_start].shape[0],
-                                              sample_len_weeks=analysis_time, no_samples=cv_samples_kalman)
+                                              cv_index_len=DF_PRICES.loc[:test_sample_start].shape[0],
+                                              sample_len_weeks=analysis_time, no_samples=cv_samples_kalman, p=p,
+                                              q=q, d=d, ma_resid=ma_residuals, arma_params=arma_params)
     # run filter on test data
     kf_xtrue, kf_xpred, kf_xfilt = run_kalman_filter(endog=[LEAD_NAME], exog=SEL_IND_NLARGEST_TICKERS.copy(),
-                                                     data=DF_RETS.loc[str(dt_start): str(PULL_END_DATE)].copy(),
-                                                     measurement_noise=measurement_noise)
+                                                     data=DF_RETS.loc[test_sample_start: PULL_END_DATE].copy(),
+                                                     measurement_noise=measurement_noise, p=p, q=q, d=d,
+                                                     ma_resid=ma_residuals, arma_params=arma_params)
 
     #### HMM ####
     # get data for CV
@@ -64,4 +83,4 @@ if __name__ == "__main__":
 
     # PLOT
     view.st_plot_output(DF_PRICES, kf_xtrue, kf_xpred, kf_xfilt, kf_conf_mat, kf_roc_score, hmm_cv_statesg,
-                        hmm_cv_states, hmm_xtrain, hmm_xtest, hmm_train_states, hmm_test_states)
+                        hmm_cv_states, hmm_xtrain, hmm_xtest, hmm_train_states, hmm_test_states, arma_true, arma_predict)
