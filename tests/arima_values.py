82cece140326895587e5ceaed1b752341555956e
Lukas Grahl <lukas.g@amkat.de>
2023-04-10 09:59:15 +0200

    added arma test

--- tests/arima_values.py
+++ tests/arima_values.py
@@ -1,3 +1,26 @@
 p = 1
 d = 0
 q = 1
+
+import os
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+from src.pull_data import load_data
+from src.toml import load_toml
+from settings import PROJECT_ROOT
+from src.arma import grid_search_arma
+from src.utils import printProgBar
+from datetime import datetime
+
+if __name__ == '__main__':
+    config = load_toml(os.path.join(PROJECT_ROOT, 'config.toml'))
+    endog = '^STOXX50E'
+    DF_PRICES, DF_RETS, SEL_IND_NLARGEST_TICKERS, LEAD_NAME = load_data('CAC 40', [endog], '2022-01-01', '2023-01-01',
+                                                                        n_largest=config['data']['n_largest_composits'],
+                                                                        no_internet=True)
+
+    p, q, d, residuals, params = grid_search_arma(5, 1, DF_RETS.iloc[-50:], endog=[endog],
+                                                  exog=SEL_IND_NLARGEST_TICKERS, sup_warnings=True)
+
+    print(p, q)
